4bf172d9089e76b010e9bae7cfbba91308811d87
Anshu <ashu.dnn@gmail.com>
2025-08-28 23:13:45 +0530

    fix: correct image import path for prompt asset

--- src/components/Landing/createPrompt.tsx
+++ src/components/Landing/createPrompt.tsx
@@ -1,4 +1,4 @@
-import prompt from '../../assets/prompt.png';
+import prompt from '../../assets/Prompt.png';
 
 type showModal = {
     setShowModal: (value: boolean) => void;
@@ -8,7 +8,6 @@ const CreatePrompt = ({ setShowModal }: showModal) => {
     return (
         <div className="bg-[#191934] rounded-3xl p-6 h-[192px] hover:scale-105 transition-transform duration-300 ease-in-out text-white flex flex-col items-center justify-center cursor-pointer hover:bg-[#2a2a4d]"
             onClick={() => setShowModal(true)}
-
         >
             <h3
                 className="text-[62px] font-semibold bg-gradient-to-r  from-[#F5F1FF] to-[#6633EE] bg-clip-text text-transparent"
@@ -22,9 +21,8 @@ const CreatePrompt = ({ setShowModal }: showModal) => {
                     created prompts
                 </p>
             </div>
-
-
-        </div>)
+        </div>
+    )
 }
 
 export default CreatePrompt
